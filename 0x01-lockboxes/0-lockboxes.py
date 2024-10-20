2a6b28311ae30a3b419d189231579aa257a09b9f
Jusca <jusca.mol@gmail.com>
2024-10-20 19:58:42 +0000

    here

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,7 +5,7 @@
 
 def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
+    (indices) to the other boxes can be unlocked given that the first
     box is unlocked.
     '''
     n = len(boxes)
